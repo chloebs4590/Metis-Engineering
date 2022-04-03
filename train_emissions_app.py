fe2ebb98574a43b4315821cac1993b41e5d540ca
Chloe Bergsma-Safar <chloebs@gmail.com>
2022-04-03 18:20:57 -0400

    Created using Colaboratory

---
+++ train_emissions_app.py
@@ -0,0 +1,43 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "name": "train_emissions_app.py",
+      "provenance": [],
+      "collapsed_sections": [],
+      "authorship_tag": "ABX9TyNRItLzaB+WONe/fMB8RuKW",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/chloebs4590/Metis-Engineering/blob/main/train_emissions_app.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "execution_count": null,
+      "metadata": {
+        "id": "8mtIpv4egDO6"
+      },
+      "outputs": [],
+      "source": [
+        ""
+      ]
+    }
+  ]
+}
