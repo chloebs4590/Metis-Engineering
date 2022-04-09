bc4696cb9adbfa5420f03fd1bbc5b2fe2a800dfc
Chloe Bergsma-Safar <chloebs@gmail.com>
2022-04-09 18:11:59 -0400

    Update emissions_app.py

--- emissions_app.py
+++ emissions_app.py
@@ -45,7 +45,7 @@ st.markdown(
     '''
     The purpose of this web app is to allow users to compare the carbon footprint of train versus car travel between cities serviced **by the same Amtrak route**.
     
-    **Note**: The destination city options reflect a simplified version of the true Amtrak network, since the web app does not account for the ability to transfer between routes.
+    **Note**: The destination city options reflect a simplified version of the true Amtrak network, since the web app design does not account for the ability to transfer between routes.
 
     **User instructions:**  
     1. Select an origin city
