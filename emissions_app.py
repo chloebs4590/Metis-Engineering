21dd54ee60f740fc8aaf0aba764b153277a12998
Chloe Bergsma-Safar <chloebs@gmail.com>
2022-04-09 18:13:22 -0400

    Update emissions_app.py

--- emissions_app.py
+++ emissions_app.py
@@ -43,7 +43,7 @@ st.title('Rail or Road?')
 
 st.markdown(
     '''
-    The purpose of this web app is to allow users to compare the carbon footprint of train versus car travel between cities serviced **by the same Amtrak route**.
+    The purpose of this web app is to allow users to compare the carbon footprint of train versus car travel between cities serviced by the same Amtrak route.
     
     **Note**: The destination city options reflect a simplified version of the true Amtrak network, since the web app design does not account for the ability to transfer between routes.
 
